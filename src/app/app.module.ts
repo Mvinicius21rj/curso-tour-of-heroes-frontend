4544740935a5ce5fd53ac85279e032b7e1ac8216
Mvinicius <mviniciusrj21@gmail.com>
2022-06-29 17:01:53 -0300

    modulo core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDatailComponent } from './hero-datail/hero-datail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { MessagesComponent } from './messages/messages.component'
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CoreModule } from './core/core.module';
-
-
-
-
-
-
 
 // Configuração do modulo
 
@@ -25,16 +16,15 @@ import { CoreModule } from './core/core.module';
     AppComponent,
     HeroesComponent,
     HeroDatailComponent,
-    MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    FlexLayoutModule,
-    CoreModule    
+    CoreModule ,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { MaterialModule } from './material/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+const COMPONENTS = [
+  MessagesComponent,
+  ToolbarComponent
+];
+const MODULES = [
+  FlexLayoutModule,
+  MaterialModule,RouterModule
+];
 
 
 @NgModule({
-  declarations: [],
-  imports: [MaterialModule],
-  exports: [MaterialModule]
+  declarations: [COMPONENTS],
+  imports: [CommonModule,MODULES],
+  exports: [COMPONENTS, MODULES],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule){
+    if (parentModule){
+      throw new Error('CoreModule has already been loaded. Import this module in the AppsModule.')
+    }
+  }
+ }
